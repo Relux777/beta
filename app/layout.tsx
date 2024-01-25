e3c3198d0429edbc9c4f9a1271d737ce43985769
ReluxCompany <relux.company@mail.ru>
2024-01-25 22:53:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,15 @@ export default function RootLayout({
         <link rel="canonical" href="https://www.ozon-gradus.ru"/>
         <meta name="robots" content="all" />
         <meta name="googlebot" content="all" />
+        <meta property="og:title" content="Работа на складе OZON - Вакансии операторов склада в компании Озон Градус" />
+        <meta
+          property="og:description"
+          content="Вакансия работника склада OZON! Компания 'Озон градус' в городе Казань приглашает операторов склада присоединиться к нашей профессиональной команде. Мы являемся ведущей компанией в своей отрасли и предлагаем множество возможностей для развития карьеры и личного профессионального роста."
+        />
+        <meta
+          property="og:image"
+          content="https://example.com/images/cool-page.https://sun9-78.userapi.com/impg/RvKUInLLcG62G_Ew7f6OYKDRltie6qIDkqdfAw/_SuL6b9ukKM.jpg?size=1280x960&quality=95&sign=80eaa3d3857a35ed6e4c7ccae934828e&c_uniq_tag=w5FL5nveHZauC2ijOWDs37GHiVw2or8vqEdB9x5IS-g&type=album"
+        />
       </head>
       <body className={myFont.className}>
         {children}

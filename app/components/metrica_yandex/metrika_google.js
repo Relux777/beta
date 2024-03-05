65810d0609a95cc5a5aa6f7664f0d4779983d99a
Relux777 <131512224+Relux777@users.noreply.github.com>
2024-03-05 20:31:29 +0300

    Update metrika_google.js

--- app/components/metrica_yandex/metrika_google.js
+++ app/components/metrica_yandex/metrika_google.js
@@ -1,16 +1,19 @@
+import { useEffect } from 'react';
+import Script from 'next/script';
 
+const GoogleAnalytics = () => {
 
-"use client";
+  useEffect(() => {
+    // Добавление отслеживания Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SCSRQ2BJFJ');
+  }, []);
 
-
-import Script from "next/script";
-
-export function MetrikaGoogle () {
   return (
-
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SCSRQ2BJFJ"></script>
-
- <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-SCSRQ2BJFJ'); </script>
-
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-SCSRQ2BJFJ" strategy="afterInteractive" />
   );
-}
+};
+
+export default GoogleAnalytics;

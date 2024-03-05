f9e52e922eec4e456f1b734d39257a425191fdb9
Relux777 <131512224+Relux777@users.noreply.github.com>
2024-03-05 20:55:55 +0300

    Update GoogleAnalytics.js

--- app/components/metrica_yandex/GoogleAnalytics.js
+++ app/components/metrica_yandex/GoogleAnalytics.js
@@ -1,1 +1,21 @@
-.
+"use client";
+
+import { useEffect } from 'react';
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+
+  useEffect(() => {
+    // Добавление отслеживания Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SCSRQ2BJFJ');
+  }, []);
+
+  return (
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-SCSRQ2BJFJ" strategy="afterInteractive" />
+  );
+};
+
+export default GoogleAnalytics;.

8090389b2aacfe7297887c75c4dc982cdadf732e
Relux777 <131512224+Relux777@users.noreply.github.com>
2024-03-11 08:04:19 +0300

    Update page.tsx

--- app/(sections)/page.tsx
+++ app/(sections)/page.tsx
@@ -34,7 +34,7 @@ export default function Index() {
             </div>
             <div className='container flex align-center flex-nowrap justify-space'>
                   <div className='text'>
-                      <h1>Работа сотрудником склада OZON Градус - г. Казань | Новая тура</h1>
+                      <h1>Работа сотрудником склада ozon - г. Казань</h1>
                       <span>Приглашаем вас присоединиться к команде профессионалов ООО &quot;Озон Градус&quot; </span><br />
                       <span>- ведущую компания в своей отрасли.</span><br />
                       <span>- предоставляем разнообразные возможности для карьерного роста.</span><br />

91552a03a30d1475c8470ce832d8a0a6c05b0678
Relux777 <131512224+Relux777@users.noreply.github.com>
2024-03-14 14:20:55 +0300

    Update page.tsx

--- app/(sections)/sections/page-1/page.tsx
+++ app/(sections)/sections/page-1/page.tsx
@@ -16,8 +16,8 @@ import photo_13 from '@/public/img/sections/mus/photo_13.jpg';
 import photo_14 from '@/public/img/sections/mus/photo_14.jpg';
 
 export const metadata: Metadata = {
-  title: 'Многоуровневая сортировка (МУС)',
-  description: 'Работа озон склады г.Казань - памятка Многоуровневая сортировка (МУС)',
+  title: 'Многоуровневая сортировка (МУС)- склад OZON  г. Казань | Новая тура',
+  description: 'Памятка - Многоуровневая сортировка (МУС). Работа озон склад г. Казань',
 }
 
 export default function GenerlInfo() {

6d0804a82c06217000ed9a24641b8ea502c9a2a1
ReluxCompany <relux.company@mail.ru>
2024-04-08 20:33:43 +0300

    main

--- app/components/accordion/page.tsx
+++ app/components/accordion/page.tsx
@@ -16,19 +16,19 @@ export default function Accordians() {
       return (
         <div className='accordions'>
           <div className='list'>
-            <div className='title' onClick={() => handleClick(0)}>Какие документы необходимы для оформления?</div>
+            <div className='title flex justify-space align-center' onClick={() => handleClick(0)}><span>Какие документы необходимы для оформления?</span><i className="fa-duotone fa-circle-down fa-bounce"></i></div>
             <div className='body'><div className="text">Паспорт (с пропиской или временной регистрацией на территории РФ)</div></div>
           </div>
           <div className='list'>
-            <div className='title' onClick={() => handleClick(1)}>Как часто выплачивается заработная плата?</div>
+            <div className='title flex justify-space align-center' onClick={() => handleClick(1)}><span>Как часто выплачивается заработная плата?</span><i className="fa-duotone fa-circle-down fa-bounce"></i></div>
             <div className='body'><div className="text">Заработная плата оплачивается 2 раза в месяц (1, 15 числа)</div></div>
           </div>
           <div className='list'>
-            <div className='title' onClick={() => handleClick(2)}>Будет ли обучение?</div>
+            <div className='title flex justify-space align-center' onClick={() => handleClick(2)}><span>Будет ли обучение?</span><i className="fa-duotone fa-circle-down fa-bounce"></i></div>
             <div className='body'><div className="text">Обязательно! Все сотрудники проходят обучение на рабочем месте с опытным наставником и полным погружением в наши процессы.</div></div>
           </div>
           <div className='list'>
-            <div className='title' onClick={() => handleClick(3)}>Сколько длится стажировка?</div>
+            <div className='title flex justify-space align-center' onClick={() => handleClick(3)}><span>Сколько длится стажировка?</span><i className="fa-duotone fa-circle-down fa-bounce"></i></div>
             <div className='body'><div className="text">Стажировка начинается после сдачи экзамена, длится она 10 дней. Стажировка оплачиваемая</div></div>
           </div>
         </div>
--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,8 @@ import Link from 'next/link';
 
 export default function Custom404() {
   return (
-    <div>
+    <div className='not_found'>
+      <i class="fa-duotone fa-circle-exclamation fa-bounce"></i>
       <h1>404 - Страница не найдена</h1>
       <p>Извините, страница, которую вы ищете, не существует.</p>
       <Link href="/">

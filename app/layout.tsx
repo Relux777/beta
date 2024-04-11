b121249da37263ecdd480740712c31433a1d0aa0
games-reflex <ytreflex@mail.ru>
2024-04-11 13:47:49 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="google-site-verification" content="25rznoP6S5Uuvc8k9IR6FaMEG7Yj8icY9OqfZz2utx4" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6594048679075960"
+   crossOrigin="anonymous"></script>
       </head>
       <body className={myFont.className}>
         {children}
